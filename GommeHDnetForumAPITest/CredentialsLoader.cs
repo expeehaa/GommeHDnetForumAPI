32c6b1118861798252a6b7b29b72aa1d64db952d
expeehaa <expeehaa@outlook.com>
2018-03-26 02:02:19 +0200

    Unignored and added GommeHDnetForumAPITest project

---
+++ GommeHDnetForumAPITest/CredentialsLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace GommeHDnetForumAPITest
+{
+    public class CredentialsLoader
+    {
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+        
+        public static CredentialsLoader GetCredentials()
+        {
+            var ass = new FileInfo(typeof(CredentialsLoader).Assembly.Location);
+            var credsFile = new FileInfo(Path.Combine(ass.DirectoryName, "creds.json"));
+            if (!credsFile.Exists)
+            {
+                using (var writer = credsFile.CreateText())
+                {
+                    writer.WriteLine(JsonConvert.SerializeObject(new CredentialsLoader(), Formatting.Indented));
+                    writer.Close();
+                }
+            }
+
+            using (var sr = new StreamReader(credsFile.OpenRead()))
+            {
+                var text = sr.ReadToEnd();
+                sr.Close();
+                return JsonConvert.DeserializeObject<CredentialsLoader>(text);
+            }
+        }
+    }
+}
---
+++ GommeHDnetForumAPITest/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GommeHDnetForumAPI;
+using GommeHDnetForumAPI.DataModels.Collections;
+using GommeHDnetForumAPI.DataModels.Entities;
+
+namespace GommeHDnetForumAPITest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var creds = CredentialsLoader.GetCredentials();
+            var forum = new Forum(creds.Username, creds.Password);
+            
+            TestFunctionalities(forum);
+            //AllUserAccountsBeforeId(forum, 202);
+
+            Console.ReadKey();
+        }
+
+
+        public static void TestFunctionalities(Forum forum)
+        {
+            var user = forum.SelfUser;
+            var user2 = forum.GetUserInfo("Dom").GetAwaiter().GetResult();
+            var cons = forum.GetConversations().GetAwaiter().GetResult();
+            var con1 = cons.First();
+            con1.DownloadMessagesAsync().GetAwaiter().GetResult();
+            Console.WriteLine(con1);
+            const string forumname = "off-topic";
+            var someForum = forum.MasterForum.SubForums.FirstOrDefault(sf => string.Equals(sf.Title, forumname, StringComparison.OrdinalIgnoreCase));
+            if (someForum != null)
+            {
+                someForum.DownloadDataAsync().GetAwaiter().GetResult();
+                Console.WriteLine(someForum.Threads.Take(5).ThreadsToString());
+                var t1 = someForum.Threads.First();
+                t1.DownloadMessagesAsync().GetAwaiter().GetResult();
+                Console.WriteLine(string.Join(", ", t1.Messages.Select(m => $"({m.ToString()})")));
+            }
+            else Console.WriteLine($"No forum with name '{forumname}' found\n");
+
+            var forums = forum.MasterForum.SubForums.ToList();
+            //foreach (var sf in forums)
+            //{
+            //    Console.WriteLine($"Downloading Forumdata {sf.Title} ...");
+            //    sf.DownloadDataAsync(1, 1).GetAwaiter().GetResult();
+            //}
+
+            var allSubForums = forums.Concat(forums.SelectMany(sf => sf.AllRealSubForums)).ToList();
+            Console.WriteLine($"\n\nAll Forums ({allSubForums.Count}):");
+            Console.WriteLine(string.Join("\n", allSubForums.Select(sf => sf.ToString())));
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Downloads and prints all accessible and verified forum users before the given id.
+        /// </summary>
+        /// <param name="forum">The Forum instance to download the users with.</param>
+        /// <param name="maxId">Exclusive maximum user ID</param>
+        public static void AllUserAccountsBeforeId(Forum forum, int maxId)
+        {
+            var userinfos = new List<UserInfo>();
+            for (var i = 1; i < maxId; i++)
+            {
+                var text = "added";
+                try
+                {
+                    var ui = forum.GetUserInfo(i).GetAwaiter().GetResult();
+                    if (ui == null) text = "null";
+                    else if (!ui.Verified.HasValue || !ui.Verified.Value) text = "not verified";
+                    if (ui?.Verified != null && ui.Verified.Value) userinfos.Add(ui);
+                }
+                catch (Exception e)
+                {
+                    text = $"exception: {e.GetType().Name}";
+                }
+                Console.WriteLine($"{i}: {text}");
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine($"Abrufbare und verifizierte Nutzer ({userinfos.Count}):");
+            Console.WriteLine(string.Join("\n", userinfos.Select(u => $"Id {u.Id,3} | Name {u.Username,-16} | Link {u.UrlPath}").ToArray()));
+            Console.WriteLine("\n\n" + string.Join("\n", userinfos.Select(u => $"Id {u.Id,3} | Name @{u.Username,-16}{(u.TimeRegistered.HasValue ? $" | Registriert seit {u.TimeRegistered.Value}" : "")}").ToArray()));
+            Console.WriteLine("\n\n" + string.Join("\n", userinfos.Select(u => $"Id {u.Id,3} | Name @{u.Username,-16}").ToArray()));
+        }
+    }
+}
