dd37c093cf656f54fb067f5e67d54497aaa53e2a
expeehaa <expeehaa@outlook.com>
2019-09-10 23:07:54 +0200

    Improve readability of ForumPaths.cs.

--- GommeHDnetForumAPI/ForumPaths.cs
+++ GommeHDnetForumAPI/ForumPaths.cs
@@ -2,67 +2,21 @@
 
 namespace GommeHDnetForumAPI {
 	public static class ForumPaths {
-		/// <summary>
-		/// Base URL equals to https://www.gommehd.net/
-		/// </summary>
 		public const string BaseUrl = "https://www.gommehd.net/";
 
-		/// <summary>
-		/// Forum URL equals to https://www.gommehd.net/forum/
-		/// </summary>
-		public const string ForumUrl = BaseUrl + ForumPath;
+		public const string ForumPath            = "forum/";
+		public const string ConversationsPath    = "forum/conversations/";
+		public const string MembersPath          = "forum/members/";
+		public const string LinkForumsPath       = "forum/link-forums/";
+		public const string ForumsPath           = "forum/forums/";
+		public const string ForumThreadsPath     = "forum/threads/";
+		public const string StatsUsersOnlinePath = "stats/users-online/";
 
-		/// <summary>
-		/// Conversations URL equals to https://www.gommehd.net/forum/conversations/
-		/// </summary>
+		public const string ForumUrl         = BaseUrl + ForumPath;
 		public const string ConversationsUrl = BaseUrl + ConversationsPath;
+		public const string MembersUrl       = BaseUrl + MembersPath;
 
-		/// <summary>
-		/// Members Url equals to https://www.gommehd.net/forum/members/
-		/// </summary>
-		public const string MembersUrl = BaseUrl + MembersPath;
-
-		/// <summary>
-		/// Forum Path equals to forum/
-		/// </summary>
-		public const string ForumPath = "forum/";
-
-		/// <summary>
-		/// Conversations Path equals to forum/conversations/
-		/// </summary>
-		public const string ConversationsPath = ForumPath + "conversations/";
-
-		/// <summary>
-		/// Members path equals to forum/members/
-		/// </summary>
-		public const string MembersPath = ForumPath + "members/";
-
-		/// <summary>
-		/// Link Forums Path equals to forum/link-forums/
-		/// </summary>
-		public const string LinkForumsPath = ForumPath + "link-forums/";
-
-		/// <summary>
-		/// Forums path equals to forum/forums/
-		/// </summary>
-		public const string ForumsPath = ForumPath + "forums/";
-
-		/// <summary>
-		/// Forum threads path equals to forum/threads/
-		/// </summary>
-		public const string ForumThreadsPath = ForumPath + "threads/";
-
-		/// <summary>
-		/// Returns the members list path with appended type query
-		/// </summary>
-		/// <param name="type">The type of the members list</param>
-		/// <returns>String representing the members list path with specified type query.</returns>
 		public static string GetMembersListTypePath(MembersListType type)
 			=> $"{MembersPath}?type={type.ToString().ToLower()}";
-
-		/// <summary>
-		/// Users online stats path equals to stats/users-online/
-		/// </summary>
-		public const string StatsUsersOnlinePath = "stats/users-online/";
 	}
 }
