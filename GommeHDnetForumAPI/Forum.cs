208e0482aaeb01602e41a24b5a04e62dd8990b88
expeehaa <expeehaa@outlook.com>
2020-09-18 21:01:12 +0200

    Change user agent to recent Firefox version on Windows 10.

--- GommeHDnetForumAPI/Forum.cs
+++ GommeHDnetForumAPI/Forum.cs
@@ -72,7 +72,7 @@ namespace GommeHDnetForumAPI {
 			_httpClient.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
 			_httpClient.DefaultRequestHeaders.Add("Referer",       ForumPaths.BaseUrl);
 			_httpClient.Timeout = TimeSpan.FromSeconds(10);
-			UserAgent           = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36";
+			UserAgent           = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0";
 		}
 
 		private void ResetCookies() {
